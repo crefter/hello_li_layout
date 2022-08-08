b37b266f152ce5a9f7ff981db0c2fb09ab233a6c
crefter <crefter1@yandex.ru>
2022-08-08 22:02:34 +0700

    created core files and app class

---
+++ lib/core/consts.dart
@@ -0,0 +1,16 @@
+abstract class Consts {
+  static const StartScreen startScreen = StartScreen();
+}
+
+class StartScreen {
+  final double startDescriptionTextSize = 30;
+  final double startButtonTextSize = 24;
+  final double startButtonBottomPadding = 36.0;
+  final double startButtonTopPadding = 36.0;
+  final double startButtonLeftRightPadding = 50.0;
+  final double startDescriptionTextLeftRightPadding = 8.0;
+  final double handLeftPadding = 109;
+  final double handRightPadding = 117;
+
+  const StartScreen();
+}
--- lib/core/presentation/screens/app.dart
+++ lib/core/presentation/screens/app.dart
@@ -1,12 +1,19 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 
-import '../../../features/start/presentation/screens/start_screen.dart';
+import '../../../features/start/presentation/screens/start_screen/start_screen.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const StartScreen();
+    return const CupertinoApp(
+      title: 'Flutter Demo',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.activeBlue,
+        scaffoldBackgroundColor: null,
+      ),
+      home: StartScreen(),
+    );
   }
 }
