2994e3b9002028134255736e433814307fb2b179
crefter <crefter1@yandex.ru>
2022-08-23 16:17:05 +0700

    changed pubspec.yaml

--- lib/core/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-abstract class Colors {
-  static const Color startScreenBackgroundColor =
-      Color.fromRGBO(255, 253, 241, 1.0);
-  static const Color marromClaro2 = Color.fromRGBO(45, 33, 4, 1.0);
-  static const Color c1 = Color.fromRGBO(253, 253, 253, 1.0);
-}
