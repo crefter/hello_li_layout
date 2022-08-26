45fd3db2e7d0debb79503f30b482b5db853c8970
crefter <crefter1@yandex.ru>
2022-08-27 01:28:38 +0700

    added new color

--- lib/core/core_colors.dart
+++ lib/core/core_colors.dart
@@ -8,6 +8,7 @@ abstract class CoreColors {
   static const Color amarelo = Color.fromRGBO(255, 191, 27, 1);
   static const Color c1 = Color.fromRGBO(253, 253, 253, 1.0);
   static const Color c3 = Color.fromRGBO(135, 135, 135, 1.0);
-  static const Color white = Colors.white;
+  static const Color white = Color.fromRGBO(245, 245, 245, 1.0);
   static const Color backgroundColor = Color.fromRGBO(245, 245, 245, 1.0);
+  static const Color c2 = Color.fromRGBO(192, 192, 192, 1.0);
 }
